af405e9b4abe1f3edf703d3b189e34d7ddef51d8
Ivan Mendoza <theeachtra2002@yahoo.com>
2019-03-16 00:09:16 -0500

    with jumbotron

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friends = [ {
+    "name" : "Bruce",
+    "photo" : "https://i.dailymail.co.uk/i/pix/2015/09/17/15/1408F800000005DC-3238473-The_booklet_that_Bruce_Lee_pictured_here_in_Enter_the_Dragon_in_-m-49_1442500221103.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+},{
+    "name" : "Chuck",
+    "photo" : "https://thumbnails.texastribune.org/vSWj02G-XIofkH6IG6KhfwH6Xuo=/850x570/smart/filters:quality(80)/https://static.texastribune.org/media/images/chuck-norris1.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
